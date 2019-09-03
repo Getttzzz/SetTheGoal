5057b929818dba9a587ff4c56612d01fdb30a3ed
YuriyGetsko <getttzzz@gmail.com>
2019-09-03 20:45:56 +0300

    Win fight with new MaterialCardView. Set new corner cut only for bottomLeft and bottomRight corners.

--- app/src/main/java/com/getz/setthegoal/ui/goals/GoalsFragment.kt
+++ app/src/main/java/com/getz/setthegoal/ui/goals/GoalsFragment.kt
@@ -3,8 +3,11 @@ package com.getz.setthegoal.ui.goals
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import com.getz.setthegoal.R
 import com.getz.setthegoal.ui.goals.pager.GoalsPagerAdapter
+import com.google.android.material.shape.CutCornerTreatment
+import com.google.android.material.shape.ShapeAppearanceModel
 import kotlinx.android.synthetic.main.fragment_goals.*
 
 class GoalsFragment : Fragment(R.layout.fragment_goals) {
@@ -13,6 +16,27 @@ class GoalsFragment : Fragment(R.layout.fragment_goals) {
         super.onViewCreated(view, savedInstanceState)
         println("GETZ.GoalsFragment.onViewCreated ---> ")
 
+        setupQuoteCard()
+
         vpGoals.adapter = GoalsPagerAdapter(childFragmentManager)
+        vpGoals.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) = Unit
+            override fun onPageScrolled(pos: Int, posOffset: Float, posOffsetPixels: Int) = Unit
+            override fun onPageSelected(position: Int) {
+                println("GETZ.GoalsFragment.onPageSelected ---> selected page=$position")
+            }
+
+        })
+
+
+    }
+
+    private fun setupQuoteCard() {
+        val shape = ShapeAppearanceModel()
+        val cutCorner =
+            CutCornerTreatment(resources.getDimensionPixelSize(R.dimen.cornerCutSize).toFloat())
+        val standardCorner = CutCornerTreatment(0f)
+        shape.setCornerTreatments(standardCorner, standardCorner, cutCorner, cutCorner)
+        cardMotivation.shapeAppearanceModel = shape
     }
 }
