f385af220f754e609d50c682cadd15a282c0cc27
YuriyGetsko <getttzzz@gmail.com>
2019-09-11 23:02:39 +0300

    Add create goal fragment.

---
+++ app/src/main/java/com/getz/setthegoal/presentationpart/feature/creategoal/CreateGoalFragment.kt
@@ -0,0 +1,8 @@
+package com.getz.setthegoal.presentationpart.feature.creategoal
+
+import com.getz.setthegoal.R
+import com.getz.setthegoal.presentationpart.core.BaseFragment
+
+class CreateGoalFragment : BaseFragment(R.layout.fragment_create_goal) {
+
+}
