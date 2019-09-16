1b00d58784937966826985d7643a0fb29993b01d
YuriyGetsko <getttzzz@gmail.com>
2019-09-16 13:50:02 +0300

    Add suggestions to create a goal.

--- app/src/main/java/com/getz/setthegoal/domainpart/core/Gandalf.kt
+++ app/src/main/java/com/getz/setthegoal/domainpart/core/Gandalf.kt
@@ -1,7 +1,7 @@
 package com.getz.setthegoal.domainpart.core
 
 /**
- * Gandalf is a fictional character and a protagonist in Tolkien's novels
+ * 💥💥💥Gandalf✨✨✨ is a fictional character and a protagonist in Tolkien's novels
  * The Hobbit and The LOTR. He is a wizard who can transform one thing to another.
  * "Mapper" name is too boring.
  * */
