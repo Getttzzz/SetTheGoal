27df70af68a800a97a75c96ef23da01d3fc7c256
YuriyGetsko <getttzzz@gmail.com>
2019-10-02 22:30:06 +0300

    Fix photo adapter item.

--- app/src/main/java/com/getz/setthegoal/presentationpart/feature/creategoal/applypicture/PhotoAdapter.kt
+++ app/src/main/java/com/getz/setthegoal/presentationpart/feature/creategoal/applypicture/PhotoAdapter.kt
@@ -4,11 +4,14 @@ import android.graphics.Paint.UNDERLINE_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.request.RequestOptions
 import com.getz.setthegoal.R
 import com.getz.setthegoal.presentationpart.core.BaseAdapter
+import com.getz.setthegoal.presentationpart.core.GlideApp
 import com.getz.setthegoal.presentationpart.entitylayer.PhotoUI
-import com.getz.setthegoal.presentationpart.util.loadPicture
+import com.getz.setthegoal.presentationpart.util.getHideableListener
 import com.getz.setthegoal.presentationpart.util.setSingleClickListener
 import kotlinx.android.synthetic.main.item_photo.view.*
 
@@ -25,10 +28,17 @@ class PhotoAdapter : BaseAdapter<PhotoUI, PhotoAdapter.VH>() {
 
     override fun onBindViewHolder(holder: VH, position: Int) {
         val photo = godList[position]
+
+        GlideApp.with(holder.view)
+            .load(photo.urls.small)
+            .apply(RequestOptions().centerCrop())
+            .listener(getHideableListener(holder.view.pbPhoto))
+            .error(R.drawable.layer_list_bee)
+            .into(holder.view.ivPhoto)
+
         holder.view.tvAuthor.paintFlags = holder.view.tvAuthor.paintFlags or UNDERLINE_TEXT_FLAG
         holder.view.tvAuthor.text = photo.userName
-        holder.view.cvPhotoRoot.isChecked = photo.isSelected
-        loadPicture(holder.view.ivPhoto, photo.urls.small)
+        holder.view.llCarpet.isVisible = photo.isSelected
     }
 
     fun select(position: Int) {
