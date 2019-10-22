9f6d48c1ae4be84513d9477f149737239ca5e392
YuriyGetsko <getttzzz@gmail.com>
2019-10-23 02:16:13 +0300

    Finish welcome screen (add texts, translations, images).

--- app/src/main/java/com/getz/setthegoal/presentationpart/feature/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/getz/setthegoal/presentationpart/feature/welcome/WelcomeFragment.kt
@@ -3,16 +3,19 @@ package com.getz.setthegoal.presentationpart.feature.welcome
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.request.RequestOptions
 import com.getz.setthegoal.R
 import com.getz.setthegoal.di.AndroidScope
 import com.getz.setthegoal.presentationpart.core.BaseFragment
 import com.getz.setthegoal.presentationpart.core.BaseVm
+import com.getz.setthegoal.presentationpart.core.GlideApp
 import com.getz.setthegoal.presentationpart.util.setSingleClickListener
 import kotlinx.android.synthetic.main.fragment_welcome.*
 import kotlinx.coroutines.delay
@@ -26,7 +29,7 @@ import org.kodein.di.generic.singleton
 import java.util.LinkedList
 
 const val WELCOME_VM_MODULE = "WELCOME_VM_MODULE"
-const val DELAY_BETWEEN_WELCOME_WORDS = 600L
+const val DELAY_BETWEEN_WELCOME_WORDS = 500L
 
 
 fun getWelcomeModule() = Kodein.Module(WELCOME_VM_MODULE) {
@@ -51,40 +54,74 @@ class WelcomeFragment : BaseFragment(R.layout.fragment_welcome) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        //Todo create pre welcome screen with this text and birds.json animation.
-        //Stop flying aimlessly
-        //Get a conscious life
-        //birds paint to gray color
-        //btn "Let's go" make accent color
-
         //These sentences will be as a guide.
         val sentences = listOf(
-            "Hello, a few words about opportunities of the app",
+            Pair(
+                getString(R.string.opportunities),
+                getString(R.string.opportunities_img)
+            ),
 
             //About separation For myself and For family
-            "It is a very important to figure out for whom you will be endeavor",
-            "I've made a two entities: for yourself and for your family",
+            Pair(
+                getString(R.string.endeavor),
+                getString(R.string.endeavor_img)
+            ),
+            Pair(
+                getString(R.string.for_family_or_for_yourself),
+                getString(R.string.for_family_or_for_yourself_img)
+            ),
 
             //About notification
-            "App can send a notification about each goal",
-            "While creating a goal you choose appropriate interval between getting a notification",
-            "You control how often the goal will disturb you",
-
-            //No category, short description
-            "You forget goals, app reminds",
-            "You lose direction, app restore",
+            Pair(
+                getString(R.string.app_sends_smart_notification),
+                getString(R.string.app_sends_smart_notification_img)
+            ),
+            Pair(
+                getString(R.string.interval_notification),
+                getString(R.string.interval_notification_img)
+            ),
 
             //About offline work
-            "App can work offline",
-            "create see remember your objectives anywhere!"
+            Pair(
+                getString(R.string.works_offline),
+                getString(R.string.works_offline_img)
+            ),
+            Pair(
+                getString(R.string.anywhere),
+                getString(R.string.anywhere_img)
+            )
         )
         vm.setSentences(sentences)
+        vm.wordLD.observe(this, Observer { word -> tvWord.text = word })
+        vm.memeLD.observe(this, Observer { memeUrl ->
+            GlideApp.with(this)
+                .load(memeUrl)
+                .apply(RequestOptions().centerInside())
+                .error(R.drawable.layer_list_bee)
+                .into(ivMeme)
+        })
+        vm.seeOthersLD.observe(this, Observer {
+            btnSeeNext.text = getString(OkayEnum.getRandomAgreement().strRes)
+            btnSeeNext.isVisible = it
+            ivMeme.isVisible = it
+        })
+        vm.thisIsTheEnd.observe(this, Observer {
+            //            tvWord.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18F)
+//            tvWord.gravity = Gravity.START
+//            tvWord.isAllCaps = false
 
-        //todo change UI according to displaying WordByWord logic
-        vm.wordLD.observe(this, Observer { word -> })
+//            val desiredHeight = resources.getDimensionPixelSize(R.dimen.svWordByWordHeight)
+//            svWelcomeWords.layoutParams.height = desiredHeight
 
-        btnSkip.setSingleClickListener { bridge.openAuthScreen() }
+            btnSeeNext.text = getString(OkayEnum.getRandomAgreement().strRes)
+            btnSeeNext.isVisible = true
+            ivMeme.isVisible = true
+            btnSeeNext.setSingleClickListener { bridge.openAuthScreen() }
+        })
+        btnSeeNext.setSingleClickListener { vm.seeNextSentence() }
+        mcvWelcomeClose.setSingleClickListener { bridge.openAuthScreen() }
+
+        vm.seeNextSentence()
     }
 }
 
@@ -95,25 +132,29 @@ class WelcomeVM : BaseVm() {
     }
 
     val wordLD = MutableLiveData<String>()
+    val memeLD = MutableLiveData<String>()
     val seeOthersLD = MutableLiveData<Boolean>()
     val thisIsTheEnd = MutableLiveData<Unit>()
 
-    private val cachedSentences = LinkedList<String>()
-    private val bufferSentences = ArrayList<String>()
+    private val cachedSentences = LinkedList<Pair<String, String>>()
+    private val bufferSentences = ArrayList<Pair<String, String>>()
 
     fun seeNextSentence() = launch {
         if (cachedSentences.isNotEmpty()) showNextSentenceWordByWord(cachedSentences)
     }
 
-    fun setSentences(sentences: List<String>) {
+    fun setSentences(sentences: List<Pair<String, String>>) {
         cachedSentences.addAll(sentences)
         bufferSentences.addAll(sentences)
     }
 
-    private suspend fun showNextSentenceWordByWord(sentences: LinkedList<String>) {
+    private suspend fun showNextSentenceWordByWord(sentences: LinkedList<Pair<String, String>>) {
         seeOthersLD.postValue(false)
 
-        val firstSentence = sentences[0]
+        val firstSentence = sentences[0].first
+        val memeUrl = sentences[0].second
+        memeLD.postValue(memeUrl)
+
         sentences.removeAt(0)
 
         val words = firstSentence.split(" ")
@@ -123,23 +164,18 @@ class WelcomeVM : BaseVm() {
 
 
             if (i == words.size - 1) {
+                //last word in sentence came
 
                 wordLD.postValue(words[i])
                 delay(DELAY_BETWEEN_WELCOME_WORDS)
-
                 wordLD.postValue(firstSentence)
+
+                //if last sentence, triggers thisIsTheEnd live data
                 if (sentences.size == 0) {
-                    val allText = StringBuilder()
-                    for (j in bufferSentences.indices) {
-                        val s = bufferSentences[j]
-                        allText.append(j + 1)
-                        allText.append(". ")
-                        allText.append(s)
-                        allText.append("\n")
-                    }
-                    wordLD.postValue(allText.toString())
                     thisIsTheEnd.postValue(Unit)
-                } else seeOthersLD.postValue(true)
+                } else {
+                    seeOthersLD.postValue(true)
+                }
             } else {
                 wordLD.postValue(words[i])
             }
